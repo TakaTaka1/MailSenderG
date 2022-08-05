9e9f56736d520ab6df162e21325b86c5c7c07457
TakaTaka1 <tk.1987.6.9@gmail.com>
2022-08-05 23:26:30 +0900

    fix sheet_repo

--- infrastructure/sheet/sheet_repository.go
+++ infrastructure/sheet/sheet_repository.go
@@ -1,32 +1,32 @@
 package sheetInfra
 
 import (
-	"os"
-	"log"
-	"encoding/json"
-	"context"
-    "google.golang.org/api/option"
-	"google.golang.org/api/sheets/v4"
-	"MailSenderG/utils"
 	"MailSenderG/data/StructData"
+	"MailSenderG/utils"
+	"context"
+	"encoding/json"
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
+	"log"
+	"os"
 )
 
 type SheetRepository struct {
 }
 
 // ポインタ型として返す
-func NewSheetRepository() *SheetRepository{
+func NewSheetRepository() *SheetRepository {
 	return &SheetRepository{}
 }
 
 // TODO
-func (sp *SheetRepository) prepSheet() *sheets.Service{	
+func (sp *SheetRepository) prepSheet() *sheets.Service {
 	file, _ := json.Marshal(Utils.SetSheetCredentials())
 	_ = os.WriteFile("./secret.json", file, 0644)
 	credential := option.WithCredentialsFile("./secret.json")
 	srv, err := sheets.NewService(context.TODO(), credential)
 	if err != nil {
-	    log.Fatal(err)
+		log.Fatal(err)
 	}
 	_ = os.Remove("./secret.json")
 	return srv
@@ -35,7 +35,7 @@ func (sp *SheetRepository) prepSheet() *sheets.Service{
 func (sp *SheetRepository) RetSheetData(sheetNameRange string) map[int]StructData.SheetData {
 	SHEET_ID := os.Getenv("SHEET_ID")
 	srv := sp.prepSheet()
-	resp, err := srv.Spreadsheets.Values.Get(SHEET_ID,sheetNameRange).Do()
+	resp, err := srv.Spreadsheets.Values.Get(SHEET_ID, sheetNameRange).Do()
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -45,11 +45,11 @@ func (sp *SheetRepository) RetSheetData(sheetNameRange string) map[int]StructDat
 	var dataMap = make(map[int]StructData.SheetData)
 	for i, row := range resp.Values {
 		dataMap[i] = StructData.SheetData{
-						row[0].(string),
-						row[1].(string),
-						row[2].(string),
-						row[3].(string),
-						row[4].(string)}
+			row[0].(string),
+			row[1].(string),
+			row[2].(string),
+			row[3].(string),
+			row[4].(string)}
 	}
 	return dataMap
 }
